bb7d52f2f5f0249ae84076afc9ea8d23fb15268f
andreacagnina <amirkal@libero.it>
2024-07-31 15:36:51 +0200

    [feat] initializing proj

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div>2</div>
+
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    1
+</template>
+
+<style lang="scss" scoped></style>
