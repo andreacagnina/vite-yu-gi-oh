9c4fc65baaf9a2ff3d796e2d9bca8008f6fb933b
andreacagnina <amirkal@libero.it>
2024-08-01 09:37:59 +0200

    [feat] changed from archtype to race

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,6 +1,10 @@
 <script>
 import { store } from '../store.js';
 export default {
+//     props: {
+// Card: Object
+//     } 
+// }
     data() {
         return {
             store,
@@ -14,7 +18,7 @@ export default {
         <div class="content border text-center bg-ocre h-100">
             <img v-for=" img in card.card_images" :src="img.image_url_small" alt="">
             <h6 class="mt-2 text-bold">{{ card.name }}</h6>
-            <p>{{ card.archetype }}</p>
+            <p>{{ card.race }}</p>
         </div>
     </div>
 
